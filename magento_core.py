33c0e4c4e0c3c773fa7cfbd5634fb65cf0919b0b
resteve <resteve@zikzakmedia.com>
2013-10-02 19:23:47 +0200

    pyflakes

--- magento_core.py
+++ magento_core.py
@@ -1,10 +1,7 @@
 #This file is part magento_manufacturer module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-
-from trytond.model import ModelView, ModelSQL, fields
-from trytond.tools import safe_eval, datetime_strftime
-from trytond.transaction import Transaction
+from trytond.model import ModelView, fields
 from trytond.pool import Pool, PoolMeta
 
 from magento import *
--- shop.py
+++ shop.py
@@ -1,12 +1,10 @@
 #This file is part magento_product module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-
-from trytond.model import ModelView, ModelSQL, fields
-from trytond.tools import safe_eval, datetime_strftime
+from trytond.model import fields
 from trytond.transaction import Transaction
-from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval, Bool
+from trytond.pool import PoolMeta
+from trytond.pyson import Eval
 
 __all__ = ['SaleShop']
 __metaclass__ = PoolMeta
