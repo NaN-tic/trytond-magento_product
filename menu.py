d5c8b5d8be6785cd5dda2d5e72a3cb99d33443c7
resteve <resteve@zikzakmedia.com>
2014-05-19 09:29:48 +0200

    Delete Catalog menu when is Magento menu

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 
 __all__ = ['CatalogMenu']
 __metaclass__ = PoolMeta
@@ -14,6 +14,14 @@ class CatalogMenu:
     magento_id = fields.Integer('External ID')
 
     @classmethod
+    def __setup__(cls):
+        super(CatalogMenu, cls).__setup__()
+        cls._error_messages.update({
+            'delete_esale_menu': 'Menu %s is available in %s Magento. '
+                'Descheck active field to dissable menu',
+        })
+
+    @classmethod
     def copy(cls, menus, default=None):
         if default is None:
             default = {}
@@ -21,3 +29,11 @@ class CatalogMenu:
         default['magento_app'] = None
         default['magento_id'] = None
         return super(CatalogMenu, cls).copy(menus, default=default)
+
+    @classmethod
+    def delete(cls, menus):
+        for menu in menus:
+            if menu.magento_id:
+                cls.raise_user_error('delete_esale_menu',
+                    (menu.rec_name, menu.magento_app.name))
+        super(CatalogMenu, cls).delete(menus)
