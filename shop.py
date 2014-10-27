572683fffd1120774ef100c504562132bbf8a0ca
resteve <resteve@zikzakmedia.com>
2014-10-27 14:48:29 +0100

    Forget changes from edffcda8588c

--- shop.py
+++ shop.py
@@ -367,7 +367,7 @@ class SaleShop:
                         if not code:
                             continue
 
-                        data = self.magento_get_prices(shop, product)
+                        data = self.magento_get_prices(product)
 
                         if app.debug:
                             message = 'Magento %s. Product: %s. Data: %s' % (
