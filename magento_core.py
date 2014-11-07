7028bd4036044adabde55cfe4f103d456a0e49ed
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:26:01 +0100

    Cleanup tests

--- magento_core.py
+++ magento_core.py
@@ -1,6 +1,6 @@
-#This file is part magento_manufacturer module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part magento_manufacturer module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.model import ModelSQL, ModelView, fields
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
