d2e0a7faa3ddd5118e8ce49583bf88e58059ab84
Raimon Esteve <raimon@nan-tic.com>
2018-02-23 13:49:14 +0100

    Set use_config_manage_stock when export product depend on manage_stock task-031926

--- product.py
+++ product.py
@@ -350,7 +350,13 @@ class Product:
 
             vals['qty'] = quantity
             vals['is_in_stock'] = '1' if quantity > 0 else '0'
-            vals['manage_stock'] = '1' if product.esale_manage_stock else '0'
+            manage_stock = '0'
+            use_config_manage_stock = '1'
+            if product.esale_manage_stock:
+                manage_stock = '1'
+                use_config_manage_stock = '0'
+            vals['manage_stock'] = manage_stock
+            vals['use_config_manage_stock'] = use_config_manage_stock
 
             # images
             # http://wiki.magmi.org/index.php?title=Image_attributes_processor
